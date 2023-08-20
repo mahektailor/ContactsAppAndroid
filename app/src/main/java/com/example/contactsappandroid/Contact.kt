e271b6ce5e9dd9bc6f3b6f8b9161bb184deea5b3
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-08-20 01:12:47 -0400

    Main activity ,Contact.ktt with package

---
+++ app/src/main/java/com/example/contactsappandroid/Contact.kt
@@ -0,0 +1,4 @@
+package com.example.contactsappandroid
+
+// Contact.kt
+data class Contact(val name: String, val phoneNumber: String, val email: String)
--- app/src/main/java/com/example/contactsappandroid/MainActivity.kt
+++ app/src/main/java/com/example/contactsappandroid/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.contactsappandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contactsappandroid.adapter.ContactAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = ContactAdapter(getSampleContacts())
+    }
+
+    private fun getSampleContacts(): List<Contact> {
+        // Create and return a list of sample contacts here
+        val contacts = mutableListOf<Contact>()
+        contacts.add(Contact("Mahek", "1234567890", "Mahek@example.com"))
+        contacts.add(Contact("Rohan", "9876543210", "Rohan@example.com"))
+        return contacts
     }
 }
